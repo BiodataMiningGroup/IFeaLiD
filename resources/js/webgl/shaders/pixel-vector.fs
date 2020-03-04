222939ea2142a4fa5d0f2bad5958bdf57d7e55b6
Martin Zurowietz <martin@zurowietz.de>
2020-03-04 13:03:52 +0100

    CLarify pixel vector shader

--- resources/js/webgl/shaders/pixel-vector.fs
+++ resources/js/webgl/shaders/pixel-vector.fs
@@ -8,10 +8,10 @@ uniform float u_texture_dimension;
 <%=TEXTURE_3D=%>
 
 void main() {
-    float tile_number =
-        floor(
-            (v_texture_position.t * u_texture_dimension + v_texture_position.s)
-            * u_texture_dimension - u_texture_dimension / 2.0
-        );
+    // Texture coordinates are in [0, 1] and mark the center of a pixel of the texture.
+    // Multiply with u_texture_dimension to get the pixel coordinates and use floor()
+    // to shift the coordinates to the left corner of the pixel.
+    float tile_number = floor(v_texture_position.t * u_texture_dimension) * u_texture_dimension + floor(v_texture_position.s * u_texture_dimension);
+
     gl_FragColor = texture3D(u_mouse_position, tile_number);
 }
