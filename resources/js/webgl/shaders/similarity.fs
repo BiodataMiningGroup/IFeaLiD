b4f6a0fa8ba321d3e79d24e857316a3419e2f26d
Martin Zurowietz <martin@zurowietz.de>
2020-03-09 10:37:05 +0100

    Remove unused code of the similarity shader

--- resources/js/webgl/shaders/similarity.fs
+++ resources/js/webgl/shaders/similarity.fs
@@ -9,26 +9,12 @@ uniform float u_normalization;
 
 out vec4 outColor;
 
-//uniform float u_channel_mask_dimension;
-//uniform float u_inv_channel_mask_dimension;
-
-//uniform sampler2D u_channel_mask;
-//uniform sampler2D u_region_mask;
-
 const vec4 ONES = vec4(1);
 const vec4 ZEROS = vec4(0);
 
 <%=SAMPLER_DEFINITION=%>
 
 void main() {
-    // if masked by the region mask, don't do anything
-    /*
-    if (texture(u_region_mask, v_texture_position).a == 0.0) {
-        gl_FragColor = ZEROS;
-        return;
-    }
-    */
-
     // angle between the two vectors
     // <A,B> = ||A|| * ||B|| * cos(angle)
     // => angle = acos(<A,B>/(||A||*||B||))
@@ -46,10 +32,6 @@ void main() {
 
     // the index of the current tile
     float tile;
-    // the texture position on the channel mask of the current tile
-    // vec2 mask_position = vec2(0);
-    // the channel mask of the current tile
-    // vec4 channel_mask;
 
     // the row-major index of the current tile on it's texture
     float index_on_referencer;
@@ -67,15 +49,6 @@ void main() {
 
     for (int i = 0; i < <%=TILES=%>; i++) {
         tile = float(i);
-        /*
-        mask_position.s = mod(tile, u_channel_mask_dimension);
-        mask_position.t = floor(tile * u_inv_channel_mask_dimension);
-        mask_position *= u_inv_channel_mask_dimension;
-        channel_mask = texture(u_channel_mask, mask_position);
-        */
-
-        // check if any channels of this tile are to be computed
-        //if (dot(channel_mask, ONES) == 0.0) continue;
 
         index_on_referencer = mod(tile, <%=TILES_PER_TEXTURE=%>);
         column = mod(index_on_referencer, <%=TILE_COLUMNS=%>);
@@ -95,13 +68,11 @@ void main() {
         // needed for DYNAMIC_SAMPLER_QUERIES
         sampler_index = floor(tile / <%=TILES_PER_TEXTURE=%>);
 
-        // get rgba of the pixel to compare; filtered by the channel mask and
-        // get rgba of the position of this pixel; filtered by the channel mask
+        // get rgba of the pixel to compare
+        // get rgba of the position of this pixel
         <%=DYNAMIC_SAMPLER_QUERIES
-        //reference = channel_mask * texture(<%=SAMPLER=%>, coords_2d_reference);
-        //current = channel_mask * texture(<%=SAMPLER=%>, coords_2d_current);
-        reference = texture(<%=SAMPLER=%>, coords_2d_reference);
-        current = texture(<%=SAMPLER=%>, coords_2d_current);
+            reference = texture(<%=SAMPLER=%>, coords_2d_reference);
+            current = texture(<%=SAMPLER=%>, coords_2d_current);
         =%>
 
         currentLength += dot(current, current);
