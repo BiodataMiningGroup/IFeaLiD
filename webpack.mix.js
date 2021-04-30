94b01785d6672dd20ececc7efd25794562780cba
Martin Zurowietz <martin@zurowietz.de>
2021-04-30 07:42:05 +0200

    Upgrade Laravel mix to fix security vulnerability

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,5 @@ const mix = require('laravel-mix');
 
 mix.disableSuccessNotifications();
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/js').vue();
+mix.sass('resources/sass/app.scss', 'public/css');
